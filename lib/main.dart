1135105ed262963a4cb22b591b9b922de4b67e6f
Toygu Çakın <toygucakin10@gmail.com>
2026-04-24 21:31:43 +0300

    Force hide keyboard on app startup/hot restart

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,11 @@ class BirikimlyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Force hide keyboard on app startup / hot restart
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FocusManager.instance.primaryFocus?.unfocus();
+    });
+
     final themeMode = ref.watch(themeProvider);
 
     return MaterialApp(
